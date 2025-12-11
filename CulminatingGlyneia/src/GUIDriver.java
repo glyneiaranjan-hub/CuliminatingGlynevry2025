92f4b4bb53b1abdc22368e9e42232ace840b388b
averyphelps <averyphelps@10.20.0.136>
2025-12-11 15:33:01 -0500

    Background, title, next button started

--- CulminatingGlyneia/src/GUIDriver.java
+++ CulminatingGlyneia/src/GUIDriver.java
@@ -1,20 +1,70 @@
-import javafx.application.Application;
-import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
+import javafx.application.Application;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+import javafx.scene.paint.Color;
+import javafx.scene.*;
+import javafx.stage.*;
+import javafx.geometry.*;
+import javafx.scene.text.*;
+
 
 public class GUIDriver extends Application {
 
 	@Override
 	public void start(Stage stage) throws Exception {
+		BorderPane bp = new BorderPane();
 		VBox root = new VBox(10);
-		Label text = new Label("HELLO THERE");
-		root.getChildren().add(text);
 		
-		Scene scene = new Scene (root,600,600);
-		stage.setScene(scene);
-		stage.show();
+		/*
+	    * The font for the text of the title
+	    */         
+	    Font titleFont = new Font("Times New Roman", 27);
+	    
+	    /*
+	    * The font for the text of the buttons and Error Message
+	    */ 
+	    Font font = new Font("Times New Roman", 15);
+		
+		/*
+	    * The Label holding the title
+	    */         
+		Label lblTitle = new Label("BOGGLE");
+	    lblTitle.setFont(titleFont);
+	    lblTitle.setUnderline(true);
+        lblTitle.setTextFill(Color.BLUE);
+        
+        /*
+         * The Label holding the error message
+         */
+         Label errorMessage = new Label ("");
+         errorMessage.setFont(font);
+         errorMessage.setUnderline(true);
+         errorMessage.setTextFill(Color.RED);
+         
+         HBox btnHolder = new HBox(10);
+         btnHolder.setStyle("-fx-background-color: lightskyblue;");
+         btnHolder.setAlignment(Pos.BOTTOM_RIGHT);
+         Button btnNext = new Button("NEXT");
+         btnNext.setFont(font);
+         
+         btnHolder.getChildren().addAll(btnNext);
+         btnHolder.setAlignment(Pos.BOTTOM_RIGHT);
+         		
+		root.getChildren().add(lblTitle);
+		
+
+        root.setAlignment(Pos.CENTER);
+
+        root.setStyle("-fx-background-color: lightskyblue;");
+        
+		bp.setCenter(root);
+		bp.setBottom(btnHolder);
+       
+        
+        Scene scene = new Scene (bp, 600, 600);
+        stage.setScene(scene);
+        stage.show();
 	}
 	
 	public static void main(String[] args) {
